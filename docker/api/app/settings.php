60397a006fdbe9ac300c7d5246e44343a1894f0f
Casey Flier <flierc@csp.edu>
2022-10-08 14:37:39 -0500

    Set up doctrine

--- docker/api/app/settings.php
+++ docker/api/app/settings.php
@@ -1,27 +1,50 @@
 <?php
+define('APP_ROOT', __DIR__);
 
-declare(strict_types=1);
+return [
+    'settings' => [
+        'slim' => [
+            // Returns a detailed HTML page with error details and
+            // a stack trace. Should be disabled in production.
+            'displayErrorDetails' => true,
 
-use App\Application\Settings\Settings;
-use App\Application\Settings\SettingsInterface;
-use DI\ContainerBuilder;
-use Monolog\Logger;
+            // Whether to display errors on the internal PHP log or not.
+            'logErrors' => true,
 
-return function (ContainerBuilder $containerBuilder) {
+            // If true, display full errors with message and stack trace on the PHP log.
+            // If false, display only "Slim Application Error" on the PHP log.
+            // Doesn't do anything when 'logErrors' is false.
+            'logErrorDetails' => true,
+        ],
 
-    // Global Settings Object
-    $containerBuilder->addDefinitions([
-        SettingsInterface::class => function () {
-            return new Settings([
-                'displayErrorDetails' => true, // Should be set to false in production
-                'logError'            => false,
-                'logErrorDetails'     => false,
-                'logger' => [
-                    'name' => 'slim-app',
-                    'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-                    'level' => Logger::DEBUG,
-                ],
-            ]);
-        }
-    ]);
-};
+        'doctrine' => [
+            // Enables or disables Doctrine metadata caching
+            // for either performance or convenience during development.
+            'dev_mode' => true,
+
+            // Path where Doctrine will cache the processed metadata
+            // when 'dev_mode' is false.
+            'cache_dir' => APP_ROOT . '/var/doctrine',
+
+            // List of paths where Doctrine will search for metadata.
+            // Metadata can be either YML/XML files or PHP classes annotated
+            // with comments or PHP8 attributes.
+            'metadata_dirs' => [APP_ROOT . '/src/Domain'],
+
+            // The parameters Doctrine needs to connect to your database.
+            // These parameters depend on the driver (for instance the 'pdo_sqlite' driver
+            // needs a 'path' parameter and doesn't use most of the ones shown in this example).
+            // Refer to the Doctrine documentation to see the full list
+            // of valid parameters: https://www.doctrine-project.org/projects/doctrine-dbal/en/current/reference/configuration.html
+            'connection' => [
+                'driver' => 'pdo_mysql',
+                'host' => 'localhost',
+                'port' => 3306,
+                'dbname' => 'freshmart',
+                'user' => 'root',
+                'password' => 'notsafeforproduction',
+                'charset' => 'utf-8'
+            ]
+        ]
+    ]
+];
---
+++ docker/api/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+
+// bootstrap.php
+
+use Doctrine\Common\Cache\Psr6\DoctrineProvider;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use UMA\DIC\Container;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$container = new Container(require __DIR__ . '/app/settings.php');
+
+$container->set(EntityManager::class, static function (Container $c): EntityManager {
+    /** @var array $settings */
+    $settings = $c->get('settings');
+
+    // Use the ArrayAdapter or the FilesystemAdapter depending on the value of the 'dev_mode' setting
+    // You can substitute the FilesystemAdapter for any other cache you prefer from the symfony/cache library
+    $cache = $settings['doctrine']['dev_mode'] ?
+        DoctrineProvider::wrap(new ArrayAdapter()) :
+        DoctrineProvider::wrap(new FilesystemAdapter(directory: $settings['doctrine']['cache_dir']));
+
+    $config = Setup::createAttributeMetadataConfiguration(
+        $settings['doctrine']['metadata_dirs'],
+        $settings['doctrine']['dev_mode'],
+        null,
+        $cache
+    );
+
+    return EntityManager::create($settings['doctrine']['connection'], $config);
+});
+
+return $container;
