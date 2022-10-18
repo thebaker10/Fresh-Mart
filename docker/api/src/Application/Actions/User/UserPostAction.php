6580a41d0a290f54de6128dbe6216bd06139d99e
Casey Flier <flierc@csp.edu>
2022-10-17 20:20:30 -0500

    Added user registration

--- docker/api/src/Application/Actions/User/UserPostAction.php
+++ docker/api/src/Application/Actions/User/UserPostAction.php
@@ -43,9 +43,9 @@ class UserPostAction extends Action
         $payload = $this->request->getParsedBody();
         $firstName = $payload['firstName'];
         $lastName = $payload['lastName'];
-        $username = $payload['username'];
+        $username = $firstName.$lastName;
         $password = $payload['password'];
-        $balance = (float) $payload['balance'];
+        $balance = (float) 50;
         $user = new User($firstName, $lastName, $username, $password, $balance);
 
         /*
@@ -63,6 +63,8 @@ class UserPostAction extends Action
             $this->logger->error($e->getMessage());
             return $this->respondWithData(['message' => 'Error while creating new user.'], 500);
         }
+
+        $this->response->withHeader('Access-Control-Allow-Origin', '*');
 
         return $this->respondWithData(['message' => 'User successfully added', 'user_id' => $user->getUserId()], 201);
     }
