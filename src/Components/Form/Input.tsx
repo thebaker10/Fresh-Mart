78c868340a49deee732c73f8a682371605b2177b
Baker Alshaban <alshabab@csp.edu>
2022-10-20 22:34:14 -0500

    fixing modal flex issues / adding form component

---
+++ src/Components/Form/Input.tsx
@@ -0,0 +1,20 @@
+type Props = {
+    label: string,
+    onChange: (text:string) => void,
+    value?: string,
+    placeHolder?: string,
+    type?: string
+}
+
+export function Input(props:Props) {
+
+
+    return (
+
+        <div className="block w-full">
+            <label className="block mb-2 text-sm font-medium text-gray-900">{props.label}</label>
+            <input className="border border-gray-700 p-2 rounded mb-5 w-full" placeholder={props.placeHolder} value={props.value} 
+            onChange={e => props.onChange(e.currentTarget.value)} type={props.type}/>
+        </div>
+    )
+}
--- src/Pages/UserSettingsPage.tsx
+++ src/Pages/UserSettingsPage.tsx
@@ -2,7 +2,6 @@ import { Nav } from "../Components/Nav/Nav";
 import { Footer } from "../Components/Footer";
 import { ChangePassword } from "../Components/UserSettingPage/ChangePassword";
 import { ChatButton } from "../Components/ChatButton";
-import { ChangeUserIcon } from "../Components/UserSettingPage/ChangeUserIcon";
 
 
 export function UserSettingsPage() {
@@ -12,7 +11,6 @@ export function UserSettingsPage() {
             
             <div className="mx-auto max-w-4xl my-5 px-5 py-5 bg-white rounded-xl">
                 
-                <ChangeUserIcon/>
 
                 
 
