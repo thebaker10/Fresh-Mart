b86bcf6f656a948ce5320a09226a69c8e12bd5c6
Baker Alshaban <alshabab@csp.edu>
2022-11-05 21:01:52 -0500

    created feature/LineItem-entity

---
+++ docker/api/src/Application/Actions/LineItem/LineItemPostAction.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Actions\LineItem;
+
+use App\Application\Actions\Action;
+use App\Domain\LineItem;
+use App\Domain\LineItem\Line_Item\Line_Item;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\TransactionRequiredException;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Slim\Factory\AppFactory;
+use Slim\Logger;
+use Slim\Psr7\Response;
+
+class LineItemPostAction extends Action
+{
+
+    private EntityManager $em;
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+        $app = AppFactory::create();
+        $c = $app->getContainer();
+        $logger = $c->get(Logger::class);
+        parent::__construct($logger);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function action(): Response
+    {
+
+        $payload = $this->request->getParsedBody();
+        $orderId = $payload['orderId'];
+        $productId = $payload['productId'];
+        $lineItemPrice = $payload['lineItemPrice'];
+        $quantity = $payload['quantity'];
+        $lineItem = new Line_Item($orderId, $productId, $lineItemPrice, $quantity);
+
+        /*
+         * CF 2022-10-13
+         * Method for adding a new entity in the database:
+         * Create a new instance of the entity's class
+         * Call $this->>em->persist($instance) to tell Doctrine ORM that we will be saving this to the database
+         * Call $this->>em->flush() to perform the insert/update
+         */
+
+        try {
+            $this->em->persist($lineItem);
+            $this->em->flush();
+        } catch (OptimisticLockException | ORMException | TransactionRequiredException $e) {
+            $this->logger->error($e->getMessage());
+            return $this->respondWithData(['message' => 'Error while creating new lineItem.'], 500);
+        }
+
+        $this->response->withHeader('Access-Control-Allow-Origin', '*');
+
+        return $this->respondWithData(['message' => 'LineItem successfully added', 'OrderId' => $lineItem->getOrderId()], 201);
+    }
+}
---
+++ docker/api/src/Domain/LineItem/LineItem.php
@@ -0,0 +1,63 @@
+<?php
+/** @noinspection PhpPropertyOnlyWrittenInspection */
+
+namespace App\Domain\LineItem;
+
+use App\Domain\Product\Product;
+use App\Domain\User\User;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\Table;
+use JetBrains\PhpStorm\ArrayShape;
+use JsonSerializable;
+
+#[Entity, Table(name: 'lineItem')]
+class Line_Item implements JsonSerializable{
+
+    #[Id, Column(type: 'integer', unique: false, nullable: false) ]
+    private int $order_id;
+
+    #[Column(type: 'integer', unique: false, nullable: false)]
+    private int $product_id;
+
+    #[Column(type: 'float', unique: false, nullable: false)]
+    private int $lineItemPrice;
+
+    #[Column(type: 'integer', unique: false, nullable: false)]
+    private string $quantity;
+
+    #[ManyToOne(targetEntity: Product::class)]
+    #[JoinColumn(name: 'product_id', referencedColumnName: 'product_id')]
+    private Product $product;
+
+    // #[ManyToOne(targetEntity: Order::class)]
+    
+    /**
+    * @return int
+    */
+    public function getOrderId(): int
+    {
+        return $this->order_id;
+    }
+
+    /**
+    * @return int
+    */
+    public function getProductId(): int
+    {
+        return $this->product_id;
+    }
+
+    #[ArrayShape(['orderId' => "int", 'productId' => "int", 'lineItemPrice' => "float", 'quantity' => "int"])] public function jsonSerialize(): array{
+        return [
+            'orderId' => $this->order_id,
+            'productId' => $this->product_id,
+            'lineItemPrice' => $this->lineItemPrice,
+            'quantity' => $this->quantity
+        ];
+    }
+}
