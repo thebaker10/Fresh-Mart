797b25a5d25fad19f50484b44530801f4a867bf2
noko12312 <42224565+noko12312@users.noreply.github.com>
2022-11-07 15:18:32 -0600

    Added basic review submit handler

--- src/Components/WriteReview.tsx
+++ src/Components/WriteReview.tsx
@@ -1,4 +1,71 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 export function WriteReview() {
+    const [alertVisible, setAlertVisible] = React.useState(false);
+    const [alertMessage, setAlertMessage] = React.useState('Something went wrong while submitting review.');
+    let rating = 5;
+
+    function submitForm(e:any){
+
+        e.preventDefault();
+        let formData = new FormData(e.target as HTMLFormElement);
+
+        //@TODO Add Loading Indicator
+
+        let data:any = {};
+        data["userId"] = 3;
+        data["productId"] = 1;
+        data["rating"] = rating;
+
+        formData.forEach((value:any,key:any) => {
+            data[key] = value;
+        });
+
+        //CF 2022-10-16
+        //Fetch is asynchronous, so it returns a Promise.  When it is resolved (the request is completed),
+        // it moves onto the then block. If an error is thrown, it is caught in the catch block.
+        fetch("http://localhost/reviews/", {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        }).then((response) => {
+            //response.json() returns a promise
+            response.json().then((body) => {
+
+            if(body.statusCode === 500) {
+                //setAlertMessage(body.data.message);
+                setAlertVisible(true);
+                
+                return;
+            }else{
+                setAlertVisible(false);
+                toggleHiddenContainer();
+            }
+            });
+        }).catch((error) => {
+            setAlertVisible(true);
+            console.log(error);
+        });
+    }
+
+    function setRating(i: any){
+        let el = document.getElementById("starSelector");
+        rating = i;
+        if(el != null){
+            for(let x = 0; x<5; x++){
+                el.children[x+1].children[0].setAttribute("class","w-5 h-5 fill-black");
+            }
+            for(let y = 0; y<i; y++){
+                el.children[y+1].children[0].setAttribute("class","w-5 h-5 fill-yellow");
+            }
+        }  
+    }
+    
+    
     return (
         
         <div>
@@ -11,38 +78,40 @@ export function WriteReview() {
                 <div id="writeReviewContainer" className="bg-white rounded-xl">
                     <div className="flex space-between w-full text-xl text-gray-300 font-bold">
                         <h1 className="flex-auto text-xl pl-6 font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">Write Review</h1>
+                        
                         <div className="flex-auto text-right pr-2">
                             <button onClick={toggleHiddenContainer}>X</button>
                         </div>
                         
                     </div>
+                    
                     <div className="p-6 pt-2 space-y-2">
-                        
-                        <form className="space-y-2" action="#">
+                        { alertVisible && <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">{alertMessage}</div> }
+                        <form className="space-y-2" action="#" onSubmit={submitForm}>
                             
                             <div id="starSelector">
                                 <p className="block mb-2 text-lg font-medium text-gray-900">Rating</p>
-                                <button type='button' onClick={() =>changeStars(1)}>
+                                <button type='button' onClick={() =>setRating(1)}>
                                         <svg className={"w-5 h-5 fill-yellow"} viewBox="0 0 24 24" >
                                             <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
                                         </svg>
                                 </button>
-                                <button type='button' onClick={() =>changeStars(2)}>
+                                <button type='button' onClick={() =>setRating(2)}>
                                         <svg className={"w-5 h-5 fill-yellow"} viewBox="0 0 24 24">
                                             <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
                                         </svg>
                                 </button>
-                                <button type='button' onClick={() =>changeStars(3)}>
+                                <button type='button' onClick={() =>setRating(3)}>
                                         <svg className={"w-5 h-5 fill-yellow"} viewBox="0 0 24 24">
                                             <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
                                         </svg>
                                 </button>
-                                <button type='button' onClick={() =>changeStars(4)}>
+                                <button type='button' onClick={() =>setRating(4)}>
                                         <svg className={"w-5 h-5 fill-yellow"} viewBox="0 0 24 24">
                                             <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
                                         </svg>
                                 </button>
-                                <button type='button' onClick={() =>changeStars(5)}>
+                                <button type='button' onClick={() =>setRating(5)}>
                                         <svg className={"w-5 h-5 fill-yellow"} viewBox="0 0 24 24">
                                             <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
                                         </svg>
@@ -81,17 +150,4 @@ function toggleHiddenContainer(){
             el.style.visibility = "hidden";
         } 
     }
-}
-
-function changeStars(i: any){
-    let el = document.getElementById("starSelector");
-    
-    if(el != null){
-        for(let x = 0; x<5; x++){
-            el.children[x+1].children[0].setAttribute("class","w-5 h-5 fill-black");
-        }
-        for(let y = 0; y<i; y++){
-            el.children[y+1].children[0].setAttribute("class","w-5 h-5 fill-yellow");
-        }
-    }  
 }
