b921d280a2b5f63e1b27c8b1e4b15149d7269bc3
noko12312 <42224565+noko12312@users.noreply.github.com>
2022-11-13 17:52:14 -0600

    fetches moved to ProductPage to reduce repeat code

--- src/Components/ProductPage/ReviewContainer.tsx
+++ src/Components/ProductPage/ReviewContainer.tsx
@@ -1,39 +1,16 @@
 import { Rating } from "../Card/Rating"
 import { WriteReview } from "../WriteReview";
 import { Review } from "./Review";
-import React, { useState, useEffect } from 'react';
 
 type Props={
     productID: string,
     stars: number,
     numOfReviews: number,
+    reviews: Array<any>,
+    users: Array<any>
 }
 
-
 export function ReviewContainer(props:Props) {
-    let [average, setAverageData] = useState<any>();
-    let [reviewData, setReviewData] = useState<any[]>([]);
-    let [userData, setUserData] = useState<any[]>([]);
-    
-    //NH 2022-11-08
-    //useEffect hook is the functional alternative for componentDidMount and componentDidUpdate functions
-    //https://reactjs.org/docs/hooks-effect.html and https://dev.to/antdp425/react-fetch-data-from-api-with-useeffect-27le
-    useEffect(() => {
-        fetch(process.env.REACT_APP_API_BASE+"/products/"+props.productID+"/reviews" )
-            .then((response) => response.json())
-            .then((data) => {
-                setReviewData(data.data) 
-                let sum = 0;
-                data.data.forEach((review: any) => sum += Number(review.rating));
-                setAverageData(Math.round(sum/data.data.length)); 
-        })
-        fetch(process.env.REACT_APP_API_BASE+"/products/"+props.productID+"/users" )
-            .then((response) => response.json())
-            .then((data) => {
-                setUserData(data.data)
-        })  
-    },[]);
-    
     return (
         <div className="mx-auto max-w-5xl mt-10 divide-gray mb-10">
             <div className="flex">
@@ -42,13 +19,13 @@ export function ReviewContainer(props:Props) {
             </div>
             
             <div className="flex item-center mt-2">
-                {average ? <Rating nStars={average}/> : <Rating nStars={0}/>}
-                <p className="ml-2">{reviewData ? reviewData.length : 0} {reviewData.length == 1 ? "review" : "reviews"}</p>
+                <Rating nStars={props.stars}/>
+                <p className="ml-2">{props.numOfReviews} {props.numOfReviews == 1 ? "review" : "reviews"}</p>
             </div>
             
             <hr id="start" className="mt-2"></hr>
             <>
-            {reviewData && reviewData.map((r, i) => <Review username={userData[i]} stars={r.rating} date = {"Reviewed on: 9/14/22"} reviewTitle = {r.reviewTitle} review={r.reviewContent}></Review>)}
+            {props.reviews.map((r, i) => <Review username={props.users[i]} stars={r.rating} date = {"Reviewed on: 9/14/22"} reviewTitle = {r.reviewTitle} review={r.reviewContent}></Review>)}
             </>
             
             <hr className="mt-2"></hr>
--- src/Pages/ProductPage.tsx
+++ src/Pages/ProductPage.tsx
@@ -14,24 +14,44 @@ type Parameters = {
 
 export function ProductPage() {
     const params = useParams<Parameters>()
+    let [average, setAverageData] = useState<any>();
+    let [reviewData, setReviewData] = useState<any[]>([]);
     let [product, setProductData] = useState<any>();
+    let [userData, setUserData] = useState<any[]>([]);
 
     //NH 2022-11-09
     //useEffect hook is the functional alternative for componentDidMount and componentDidUpdate functions
     //https://reactjs.org/docs/hooks-effect.html and https://dev.to/antdp425/react-fetch-data-from-api-with-useeffect-27le
     useEffect(() => {
+        // Gets reviews for given productID and stores it in reviewData
+        // Calculates review score average and stores it in average
+        fetch(process.env.REACT_APP_API_BASE+"/products/"+params.productID+"/reviews" )
+            .then((response) => response.json())
+            .then((data) => {
+                setReviewData(data.data) 
+                let sum = 0;
+                data.data.forEach((review: any) => sum += Number(review.rating));
+                setAverageData(Math.round(sum/data.data.length)); 
+        })
+        // Gets product data for given productID and stores it in product
         fetch(process.env.REACT_APP_API_BASE+"/products/"+params.productID)
         .then((response) => response.json())
         .then((data) => {
             setProductData(data.data);
         })
+        // Gets user data (Only First+Last name) and stores it in userData 
+        fetch(process.env.REACT_APP_API_BASE+"/products/"+params.productID+"/users" )
+            .then((response) => response.json())
+            .then((data) => {
+                setUserData(data.data)
+        })  
     },[]);
 
     return (
         <div className="bg-lightGray">
             <Nav></Nav>
-            {product ? <ProductDetails name={product.productName} stars={4} numOfReviews={100} price={product.product_price} msrp={product.product_msrp} description={"Test"} /> : <ProductDetailsPlaceholder></ProductDetailsPlaceholder>}
-            <ReviewContainer productID = {""+params.productID} stars={Math.round(Math.random() * 5)} numOfReviews={Math.round(Math.random() * 500)}></ReviewContainer>
+            {product ? <ProductDetails name={product.productName} stars={average} numOfReviews={reviewData.length} price={product.product_price} msrp={product.product_msrp} description={"Test"} /> : <ProductDetailsPlaceholder></ProductDetailsPlaceholder>}
+            {product ? <ReviewContainer productID = {product.productId} reviews={reviewData} users={userData} stars={average} numOfReviews={reviewData.length}></ReviewContainer>:<p>none</p>}
             <Footer></Footer>
             <TawkTo></TawkTo>
         </div>
