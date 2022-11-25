e711b794acc07c81652ac643550a24173c1049e4
Casey Flier <flierc@csp.edu>
2022-11-25 09:57:07 -0600

    Add attributes to form for autofill

--- src/Components/Form/Input.tsx
+++ src/Components/Form/Input.tsx
@@ -4,7 +4,8 @@ type Props = {
     value?: string,
     placeHolder?: string,
     type?: string,
-    className?: string
+    className?: string,
+    attrId?: string
 }
 
 export function Input(props:Props) {
@@ -14,7 +15,7 @@ export function Input(props:Props) {
 
         <div className={`block  ${props.className}`}>
             <label className="block mb-2 text-sm font-medium text-gray-900">{props.label}</label>
-            <input className="border border-gray-700 p-2 rounded mb-5 w-full" placeholder={props.placeHolder} value={props.value} 
+            <input id={props.attrId} name={props.attrId} className="border border-gray-700 p-2 rounded mb-5 w-full" placeholder={props.placeHolder} value={props.value}
             onChange={e => props.onChange(e.currentTarget.value)} type={props.type}/>
         </div>
     )
--- src/Components/UserSetting/UserSettingsModal.tsx
+++ src/Components/UserSetting/UserSettingsModal.tsx
@@ -1,12 +1,27 @@
 import { Input } from "../Form/Input"
 import { UserBanner } from "./UserBanner"
+import {useEffect} from "react";
 
 type Props = {
     visible: boolean,
-    onClose: () => void
+    onClose: () => void,
+    userData?: UserData
 }
 
 export function UserSettingsModal(props: Props) {
+
+    //Replace with call to global user object
+    useEffect(() => {
+        let matches = /freshMartUserId=(\d+)/g.exec(document.cookie);
+        let user_id = (matches !== null) ? matches[1] : '';
+        fetch(process.env.REACT_APP_API_BASE + '/users/' + user_id, {
+            credentials: 'include'
+        }).then((response) => {
+            response.json().then((body) => {
+                props.userData = body.data;
+            });
+        });
+    });
 
     if (!props.visible) return null
     return (
@@ -24,19 +39,19 @@ export function UserSettingsModal(props: Props) {
 
                 <div className="mt-4 grid grid-cols-2 gap-3">
 
-                    <Input label="First Name" placeHolder="Jack" onChange={console.log} />
-                    <Input label="Last Name" placeHolder="Green" onChange={console.log} />
+                    <Input label="First Name" attrId="fName" placeHolder="Jack" onChange={console.log} />
+                    <Input label="Last Name" attrId="lName" placeHolder="Green" onChange={console.log} />
 
 
-                    <Input className="col-span-2" type="email" label="Email" placeHolder="email@example.com" onChange={console.log} />
-                    <Input className="col-span-2" type="password" label="Current password"  onChange={console.log} />
-                    <Input type="password" label="New Password"  onChange={console.log} />
-                    <Input type="password" label="Confirm Password"  onChange={console.log} />
-                    <Input className="col-span-2" type="text" label="Street Address" placeHolder="10 Street XYZ 654" onChange={console.log} />
-                    <Input type="text" label="City" placeHolder="Minneapolis" onChange={console.log} />
-                    <Input type="text" label="State" placeHolder="MN" onChange={console.log} />
-                    <Input type="text" label="Zip Code" placeHolder="55555" onChange={console.log} />
-                    <Input type="text" label="Country" placeHolder="USA" onChange={console.log} />
+                    <Input className="col-span-2" type="email" label="Email" attrId="email"  placeHolder="email@example.com" onChange={console.log} />
+                    <Input className="col-span-2" type="password" label="Current password" attrId="password"   onChange={console.log} />
+                    <Input type="password" label="New Password" attrId="newPassword"   onChange={console.log} />
+                    <Input type="password" label="Confirm Password"  attrId="newPasswordConfirm"  onChange={console.log} />
+                    <Input className="col-span-2" type="text" label="Street Address" attrId="address"  placeHolder="10 Street XYZ 654" onChange={console.log} />
+                    <Input type="text" label="City" placeHolder="Minneapolis" attrId="city"  onChange={console.log} />
+                    <Input type="text" label="State" placeHolder="MN" attrId="state"  onChange={console.log} />
+                    <Input type="text" label="Zip Code" placeHolder="55555" attrId="zipCode"  onChange={console.log} />
+                    <Input type="text" label="Country" placeHolder="USA" attrId="country"  onChange={console.log} />
 
                 </div>
 
@@ -52,7 +67,20 @@ export function UserSettingsModal(props: Props) {
 
             </form>
         </div>
-
-
     )
+}
+
+//Replace when user information branch is merged
+interface UserData {
+    userId: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    balance: string;
+    shoppingCart: any[];
+    address: string;
+    city: string;
+    state: string;
+    zip: string;
+    country: string;
 }
