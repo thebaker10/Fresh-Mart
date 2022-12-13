1ff42ab187cc4c9d6dfb876b74e47124fb4163e1
noko12312 <42224565+noko12312@users.noreply.github.com>
2022-12-12 18:54:52 -0600

    get line items from order

--- docker/api/src/Application/Actions/Order/OrderAction.php
+++ docker/api/src/Application/Actions/Order/OrderAction.php
@@ -5,26 +5,37 @@ declare(strict_types=1);
 namespace App\Application\Actions\Order;
 
 use App\Application\Actions\Action;
+use App\Domain\Order\Order;
 use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Factory\AppFactory;
+use Slim\Logger;
+use Doctrine\ORM\EntityManager;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class OrderAction extends Action
 {
     /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+        $app = AppFactory::create();
+        $c = $app->getContainer();
+        $logger = $c->get(Logger::class);
+        parent::__construct($logger);
+    }
+
+
+    /**
      * {@inheritdoc}
      */
     protected function action(): Response
-    {
-        
-
-        $fakeOrderResponse = [
-            [
-                'orderId' => 1,
-                'userId' => 1,
-                'orderDate' => 1,
-                'orderPrice' => 5,
-            ]
-        ];
-
-        return $this->respondWithData($fakeOrderResponse);
+    {   
+        $slug = $this->resolveArg('orderSlug');
+        $order = $this->em->getRepository(Order::class)->findBy(array('order_id' => $slug));
+        return $this->respondWithData($order);
     }
 }
