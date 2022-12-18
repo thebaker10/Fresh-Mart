d911668d5305b399ad2413023a05764b2c69101f
noko12312 <42224565+noko12312@users.noreply.github.com>
2022-12-17 21:04:54 -0600

    fixed merge issue

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -149,7 +149,6 @@ export function Card(props: Props) {
   }
 
   return (
-
     <div className="p-5 relative">
       <div className="flex max-w-md m-auto bg-white shadow-lg rounded-lg overflow-hidden cursor-pointer" onClick={goToProductPage}>
         <div className="w-1/3">
@@ -162,39 +161,20 @@ export function Card(props: Props) {
 
             <div className="h-auto max-h-16 w-auto">
               <p id="truncatedText" className="mt-2 text-black text-sm">{props.description}</p>
-                </div>
-              </div>
-              <div className="flex-end">
-                <div className="flex item-center mt-2">
-                  {average ? <Rating nStars={average}/> : <Rating nStars={0}/>}
-                </div>
-                <div className="flex  item-center justify-between mt-3">
-                  {props.showDeal?
-                  <h1 className={`text-black font-bold text-xl line-through decoration-red-600 decoration-[2.5px] decoration-wavy`}>${props.msrp?.toFixed(2)}</h1>:null}
-                  <h1 className={`text-black font-bold text-xl`}>${props.price.toFixed(2)}</h1>
-                  <button className="px-3 py-2 min-w-[50%] bg-green text-white text-xs font-bold uppercase rounded" onClick={(event) => addToCart(event)}>{loading ? <FontAwesomeIcon icon={faSpinner} spinPulse={true} color={'white'} size={"1x"} /> : added || isInCart ? "In Cart" : "Add To Cart"}</button>
-                </div>
-              </div>
-              
             </div>
           </div>
           <div className="flex-end">
             <div className="flex item-center mt-2">
-              {average ? <Rating nStars={average} /> : <Rating nStars={0} />}
+              {average ? <Rating nStars={average}/> : <Rating nStars={0}/>}
             </div>
             <div className="flex  item-center justify-between mt-3">
-              <h1 className="text-black font-bold text-xl">${props.price.toFixed(2)}</h1>
-
-
-
-
-
-
-
+              {props.showDeal?
+              <h1 className={`text-black font-bold text-xl line-through decoration-red-600 decoration-[2.5px] decoration-wavy`}>${props.msrp?.toFixed(2)}</h1>:null}
+              <h1 className={`text-black font-bold text-xl`}>${props.price.toFixed(2)}</h1>
               <button className="px-3 py-2 min-w-[50%] bg-green text-white text-xs font-bold uppercase rounded" onClick={(event) => addToCart(event)}>{loading ? <FontAwesomeIcon icon={faSpinner} spinPulse={true} color={'white'} size={"1x"} /> : added || isInCart ? "In Cart" : "Add To Cart"}</button>
             </div>
           </div>
-
+          
         </div>
       </div>
       <button onClick={addToFavorites}
