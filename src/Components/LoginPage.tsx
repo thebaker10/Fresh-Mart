ac98f547969391e5708ac616c410ff3af786d678
Casey Flier <flierc@csp.edu>
2022-12-17 19:42:06 -0600

    Replace anchor tags to react-dom links

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -99,7 +99,7 @@ export function LoginPage() {
                             <button type="submit" className="w-full text-white bg-blue hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
                                 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="/RegistrationPage" className="font-medium text-primary-600 hover:underline dark:text-blue">Sign up</a>
+                                Don’t have an account yet? <Link to="/RegistrationPage" className="font-medium text-primary-600 hover:underline dark:text-blue">Sign up</Link>
                             </p>
                         </form>
                     </div>
--- src/Components/RegistrationPage.tsx
+++ src/Components/RegistrationPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export function RegistrationPage() {
 
@@ -92,7 +92,7 @@ export function RegistrationPage() {
                             </div>
                             <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={() => setAlertVisible(false)}>Create an account</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Already have an account? <a href="/LoginPage" className="font-medium text-primary-600 hover:underline dark:text-blue">Login here</a>
+                                Already have an account? <Link to="/LoginPage" className="font-medium text-primary-600 hover:underline dark:text-blue">Login here</Link>
                             </p>
                         </form>
                     </div>
