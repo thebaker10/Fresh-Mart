9b2c11afba24611d96490ebe7fe75fa758207e7b
Casey Flier <flierc@csp.edu>
2022-12-18 15:44:34 -0600

    delete cookie with path and domain

--- src/Services/Util.ts
+++ src/Services/Util.ts
@@ -21,6 +21,6 @@ export function removeCookies() {
   var multiple = res.split(";");
   for (var i = 0; i < multiple.length; i++) {
     var key = multiple[i].split("=");
-    document.cookie = key[0] + " =; expires = Thu, 01 Jan 1970 00:00:00 UTC";
+    document.cookie = key[0] + " =; expires = Thu, 01 Jan 1970 00:00:00 UTC; path = /; domain = fresh-market-project.com";
   }
 }
